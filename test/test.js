41aeaeae6bf1f0082642015e08b2a6502090a080
timocodex <timothy.og.siahaan@gmail.com>
2017-02-13 11:12:30 +0700

    initial commit

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var chai = require('chai').should()
+var
+describe('tes fizzbuzz',function(){
+  it('seharusnya mengembalikan mkfizbuz function',function(){
+    fizzbuzz.mkfizbuz.should.be.ok
+  })
+})
