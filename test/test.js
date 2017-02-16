070d83458fd34869dc8057d3a0791f2e6c87f6cf
timocodex <timothy.og.siahaan@gmail.com>
2017-02-16 12:43:51 +0700

    done3

--- test/test.js
+++ test/test.js
@@ -13,25 +13,6 @@ describe('tes fizzbuzz',function(){
   it('seharusnya mengembalikan mkfizbuz function',function(){
     fizzbuzz.mkfizbuz.should.be.ok
   })
-  it('seharusnya mengembalikan fizz jika habis dibagi 3 ,buzz jika habis dibagi 5 dan fizzbuzz jika habis dibagi 5 dan 3',function(){
-    fizzbuzz.mkfizbuz(15).should.equalTo([ 1,
-  2,
-  'fizz',
-  4,
-  'buzz',
-  'fizz',
-  7,
-  8,
-  'fizz',
-  'buzz',
-  11,
-  'fizz',
-  13,
-  14,
-  'fizzbuzz'
-]
-
-)
 
   })
 })
